550b25592374664ed61b27282fbf17ed8761d6ec
iampraveenanthony <yourspraveenleo11@gmail.com>
2024-10-13 18:50:28 +0530

    update files

--- src/AmaNav1.js
+++ src/AmaNav1.js
@@ -1,15 +1,12 @@
 import React from "react";
-import "./ama-nav.css"
-import Logo from "./SIteLogo.png"
+import "./ama-nav.css";
+import Logo from "./SIteLogo.png";
 
 const AmaNav1 = () => {
   return (
     <div className="ama-nav1">
       <div className="nav1-div1">
-        <img
-          src={Logo}
-          alt="logo"
-        />
+        <img src={Logo} alt="logo" />
         <div className="nav1-div1-deliver">
           <a href="#" className="nav1-div1-deliver-up">
             Deliver to
@@ -21,9 +18,33 @@ const AmaNav1 = () => {
       </div>
 
       <div className="nav1-div2">
-        <a href="#" className="nav1-div2-left">
-          Deals▼
-        </a>
+        <select className="nav1-div2-left">
+            <option value="frontend">All Categories</option>
+            <option value="backend">Alex Skills</option>
+            <option value="ai">Amazon Devices</option>
+            <option value="blockchain">Amazon Fashion</option>
+            <option value="sharemarket">Amazon Fresh</option>
+            <option value="trading">Amazon Fresh Meat</option>
+            <option value="donate">Amazon Pharmacy</option>
+            <option value="donate">Appliances</option>
+            <option value="donate">Apps & Games</option>
+            <option value="donate">Audible Audiobooks</option>
+            <option value="donate">Baby</option>
+            <option value="donate">Beauty</option>
+            <option value="donate">Books</option>
+            <option value="donate">Car & Motorbike</option>
+            <option value="donate">Clothing & Accessories</option>
+            <option value="donate">Collectibles</option>
+            <option value="donate">Computers & Accessories</option>
+            <option value="donate">Deals</option>
+            <option value="donate">Electronics</option>
+            <option value="donate">Furniture</option>
+            <option value="donate">Garden & Outdoors</option>
+            <option value="donate">Gift cards</option>
+            <option value="donate">Grocery & Gourmet Foods</option>
+            <option value="donate">Health & Personal Care</option>
+            <option value="donate">Home & Kitchen</option>
+        </select>
         <input type="text" placeholder="Search Amazon" />
         <a href="#" className="nav1-div2-right">
           <i class="fa-solid fa-magnifying-glass"></i>
